aeaef6ba4d9bb6c9a65b9d801cc900e356e309a2
JULIA APARECIDA DA SILVA FONSECA <23111808@CFP127ALUNO.LOCAL>
2024-03-19 17:26:14 -0300

    finalizando

--- acme_filmes/app.js
+++ acme_filmes/app.js
@@ -26,10 +26,13 @@ const app = express()
 app.use((request, response, next) => {
 
     response.header('Access-Control-Allow-Origin', '*');
-    response.header('Access-Control-Allow-Methods', 'GET, POST');
+    response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     app.use(cors());
     next();
 })
+
+//cria um objeto para definir o tipo de dados que ira chegar no body (json)
+const bodyParserJSON = bodyParser.json();
 
 /**
  **************** import dos arquivos internos do projeto************/
@@ -58,6 +61,10 @@ app.get('/v1/acme/filme/:id', cors(), async (request, response, next) => {
 
 app.get('/v2/acmefilmes/filmes', cors (), async function(request, response, next){
 
+    let contentType = request.headers['content-type'];
+
+    console.log(contentType);
+
     let dadosFilmes = await controllerFilmes.getListarfilmes();
     
     if(dadosFilmes){
@@ -76,6 +83,18 @@ app.get('/v2/acmefilmes/filme/:id', cors(),async function(request,response,next)
 
     response.status(dadosFilme.status_code);
     response.json(dadosFilme);
+})
+
+app.post('/v2/acmefilmes/filmes' , cors(), bodyParserJSON, async function(request,response, next){
+
+    contentType = request.headers['content-type']
+
+    let dadosBody = request.body;
+
+    let resultDados = await controllerFilmes.setInserirNovoFilme(dadosBody, contentType);
+
+    response.status(resultDados.status_code);
+    response.json(resultDados);
 })
 
 app.listen(8080, () => {
